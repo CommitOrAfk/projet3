e74d813d49c59bd69dea01b9d8872e5ab2667bd0
Styl-Pierre <pierre.aznar.12@gmail.com>
2015-02-19 15:10:07 +0100

    Première version pour saisir un compte
    permet de saisir un compte. Gros changements dans les classes. OK pour
    test

---
+++ V0.1/client.cpp
@@ -0,0 +1,80 @@
+#include "client.h"
+
+/*Client::Client()
+{
+    identifiant=(int)(this);
+    if(identifiant < 0)
+    {
+        identifiant=identifiant * -1;
+    }
+    codesecret=0000;
+    nom="MARTIN";
+    prenom="Paul";
+    adresse="15 rue de la paix";
+}*/
+
+Client::Client(int id, int code, string nom, string prenom, string adresse)
+{
+    string poubelle;        // on cree un string qui nous servira juste pour regler un probleme de tampon memoire
+    identifiant=id;
+    this->identifiant = (int)(this);
+    if(this->identifiant < 0)
+    {
+        this->identifiant = this->identifiant * -1;
+    }
+
+    cout << "code secret : ";
+    cin >> codesecret;
+    this->codesecret=code;
+    getline(cin,poubelle);  // getline pour regler le probleme de tampon
+    cout << "nom : ";
+    getline(cin,nom);
+    this->nom=nom;
+    cout << "prenom : ";
+    getline(cin,prenom);
+    this->prenom=prenom;
+    cout << "adresse : ";
+    getline(cin,adresse);
+    this->adresse=adresse;
+
+}
+
+void Client::Afficher(ostream &out) const
+{
+    out << "id : " << identifiant << "\t" << setfill('0')  << "code : " << setw(4) << codesecret << endl;
+    out << "nom : " << nom << "\t" << "prenom : " << prenom << endl;
+    out << "adresse : " << adresse << endl;
+}
+
+void Client::Saisir(istream &in)
+{
+    string poubelle;        // on cree un string qui nous servira juste pour regler un probleme de tampon memoire
+
+    cout << "Saisir les informations relatives au client : \n";
+    identifiant = (int)(this);
+    if(this->identifiant < 0)
+    {
+        this->identifiant = this->identifiant * -1;
+    }
+    cout << "code secret : ";
+    cin >> codesecret;
+    getline(cin,poubelle);  // getline pour regler le probleme de tampon
+    cout << "nom : ";
+    getline(cin,nom);
+    cout << "prenom : ";
+    getline(cin,prenom);
+    cout << "adresse : ";
+    getline(cin,adresse);
+}
+
+ostream &operator<<(ostream &out, const Client &C)
+{
+    C.Afficher(out);
+    return out;
+}
+
+istream &operator>>(istream &in, Client &C)
+{
+    C.Saisir(in);
+    return in;
+}
---
+++ V0.1/main.cpp
@@ -0,0 +1,37 @@
+/** main.cpp juste pour tester
+test date uniquement pour l'instant
+**/
+#include "date.h"
+#include "client.h"
+#include "compte.h"
+
+int main()
+{
+/*Date D1;
+Date D2(18, 2, 2015);
+
+D1.Afficher(cout);
+D2.Afficher(cout);
+
+if(D1<D2)
+{
+    cout<<"toto \n";
+}
+else
+{
+    cout<<"pa toto \n";
+}
+
+D2 = D2+5;
+D2.Afficher(cout);
+
+Client C1(1111, 0000, "Aznar", "Pierre", "24, rue de machin Millau");
+C1.Afficher(cout);
+C1.Saisir(cin);
+C1.Afficher(cout);*/
+
+Compte Co1(111, 1234.56);
+Co1.Afficher();
+
+return 0;
+}
