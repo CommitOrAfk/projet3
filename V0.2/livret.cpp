e2cbc515339ffed8faf4fdd3cf781a8cffb1702d
Styl-Pierre <pierre.aznar.12@gmail.com>
2015-02-23 15:28:03 +0100

    Classe de Jordan corrigée

--- V0.2/livret.cpp
+++ V0.2/livret.cpp
@@ -11,7 +11,6 @@ Livret::Livret(): Compte(id, solde)
 
 Livret::~Livret()
 {
-    cout<<"Destructeur du livret"<<endl;
 }
 
 void Livret::Ajouter()
@@ -36,3 +35,41 @@ double Livret::CalculInteret()
     // Il faudrait aussi gérer le transfert d'argent entre le compte et le livret
     return (solde*taux_remuneration)/24*12;
 }
+
+void Livret::Afficher() const
+{
+    cout<<"*** Livret Epargne ***\n";
+    Compte::Afficher();
+    cout<<"Taux de rémunération: "<< setprecision(2) <<this->taux_remuneration<<endl;
+    cout<<"Le plafond: "<< setprecision(2) <<this->plafond<<endl;
+}
+
+void Livret::Saisir()
+{
+    this->id = (int)(this);
+    if(this->id < 0)
+    {
+        id = id*-1;
+    }
+    this->taux_remuneration = 0.0125;
+    this->plafond = 22000;
+    this->isOpen=true;
+
+}
+
+void Livret::Cloturer()
+{
+    //Appel de la fonction dans compte, pas de surcharge
+}
+
+double Livret::Consulter()
+{
+    return this->solde;
+}
+
+void Livret::EstOuvert()
+{
+    //Appel de la fonction dans compte, pas de surcharge
+}
+
+
